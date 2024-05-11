68bb0f1d03eebe8ce74a31b29c94d201140aac17
Atakan Başaran <basaranatakan10@gmail.com>
2024-05-11 23:05:17 +0300

    Initial Commit

---
+++ MyAI/MyAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAIApp.swift
+//  MyAI
+//
+//  Created by Atakan Başaran on 11.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
